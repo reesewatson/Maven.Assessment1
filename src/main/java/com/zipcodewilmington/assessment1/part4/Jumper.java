b6e2d790e459d7a45e92d9fbe55ae954a4c69636
Reese Watson <sherrese.watson@gmail.com>
2019-02-18 12:33:37 -0500

    eleventh commit, moving onto jumper

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,6 +6,9 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
+
         return -1;
     }
+
+
 }
--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -3,6 +3,17 @@ package com.zipcodewilmington.assessment1.part5;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
-    }
+
+            char[] charArray = input.toCharArray();
+            int count = 0;
+            int i = 0;
+
+            while (i < charArray.length) {
+                if(charArray[i] == charArray[charArray.length - i - 1]) {
+                    count++;
+                    i++;
+                }
+            }
+            return count;
+
 }
