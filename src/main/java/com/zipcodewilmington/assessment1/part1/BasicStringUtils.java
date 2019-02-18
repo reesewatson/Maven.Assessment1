9fbcff1c385cf2374f341d2aad136e2891aaaf43
Reese Watson <sherrese.watson@gmail.com>
2019-02-18 09:57:48 -0500

    checkpointing work, third checkpoint, moving on to integerarrayutils

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -70,6 +70,20 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+
+        char[] chars = str.toCharArray();
+            for (int i = 0; i < chars.length; i++)
+            {
+                char c = chars[i];
+                if (Character.isUpperCase(c))
+                {
+                    chars[i] = Character.toLowerCase(c);
+                }
+                else if (Character.isLowerCase(c))
+                {
+                    chars[i] = Character.toUpperCase(c);
+                }
+            }
+            return new String(chars);
     }
-}
+
